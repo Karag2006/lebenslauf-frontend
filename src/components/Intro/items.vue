98465a1cf5477b75dc7b9dc0c98110da42fa7468
Martin Richter <martin.richter2006@gmail.com>
2021-06-08 21:34:00 +0200

    introContent.json, style.css und 2 weitere dateien aktualisiert...

---
+++ src/components/Intro/items.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="intro--text">
+        <text-item 
+            v-for="item in items"
+            :key="item.id"
+            :item="item">
+        </text-item>
+    </div>
+</template>
+
+<script>
+import textItem from './singleItem.vue'
+export default {
+    components:{
+        textItem
+    },
+    props:{
+        items: Array
+    }
+}
+</script>
---
+++ src/components/Intro/singleItem.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="text--item">
+        <p class="text--item--title">{{item.title}} : </p>
+        <p class="text--item--content"> {{item.content}} </p>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        item: Object
+    }
+}
+</script>
