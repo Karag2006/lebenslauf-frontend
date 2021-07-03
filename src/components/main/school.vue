4a7bcfb24e0e9e65eae13a8dbf016c6dc33ad67e
Martin Richter <martin.richter2006@gmail.com>
2021-07-03 11:34:22 +0200

    school.vue und schoolItem.vue aktualisiert

--- src/components/main/school.vue
+++ src/components/main/school.vue
@@ -1,22 +1,51 @@
 <template>
     <div class="school">
-        <h4 class="school--title"> {{school.title}} </h4>
+        <h4 v-if="!loggedIn" class="school--title"> {{item.title}} </h4>
+        <h4 v-else-if="!editmode" class="clickable school--title" @click="edit"> {{item.title}} </h4>
+        <edit-item
+            v-else
+            :names="['title']"
+            :values="item"
+            :location="location"
+            @cancel="cancel"
+            @changed="cancel"
+        ></edit-item>
         <school-item
-            v-for="item in school.schoolItems"
-            :key="item.id"
-            :item="item"
+            v-for="schoolItem in item.schoolItems"
+            :key="schoolItem.id"
+            :item="schoolItem"
+            :location="location + '.schoolItems.' + schoolItem.id"
+            :loggedIn="loggedIn"
         ></school-item>
     </div>
 </template>
 
 <script>
 import schoolItem from './schoolItem.vue'
+import editItem from '../admin/edit/editItem.vue'
+
 export default {
     components:{
-        schoolItem
+        schoolItem,
+        editItem
     },
     props:{
-        school: Object
-    }
+        item: Object,
+        location: String,
+        loggedIn: Boolean
+    },
+    data() {
+        return {
+            editmode: false
+        }
+    },
+    methods: {
+        edit(){
+            this.editmode = true
+        },
+        cancel(){
+            this.editmode = false
+        }
+    },  
 }
 </script>
--- src/components/main/schoolItem.vue
+++ src/components/main/schoolItem.vue
@@ -1,18 +1,54 @@
 <template>
-    <div class="studies--item">
-        <p>{{item.institution}}<br><span class="accent">{{item.timeFrame}}</span></p>
+    <div v-if="!loggedIn" class="school--item">
+        <div class="school--item--head">
+            <p>{{item.institution}}<br><span class="accent">{{item.timeFrame}}</span></p>
+        </div>
         <ul>
             <li>{{item.class}}</li>
         </ul>
     </div>
+    <div v-else-if="!editmode" class="clickable school--item" @click="edit">
+        <div class="school--item--head">
+            <p>{{item.institution}}<br><span class="accent">{{item.timeFrame}}</span></p>
+        </div>
+        <ul>
+            <li>{{item.class}}</li>
+        </ul>
+    </div>
+    <edit-item 
+        v-else
+        :names="['institution', 'timeFrame', 'class']"
+        :values="item"
+        :location="location"
+        @cancel="cancel"
+        @changed="cancel"
+    ></edit-item>
 </template>
 
 <script>
-
+import editItem from '../admin/edit/editItem.vue'
 
 export default {
+    components:{
+        editItem
+    },
     props:{
-        item: Object
-    }
+        item: Object,
+        location: String,
+        loggedIn: Boolean
+    },
+    data() {
+        return {
+            editmode: false
+        }
+    },
+    methods: {
+        edit(){
+            this.editmode = true
+        },
+        cancel(){
+            this.editmode = false
+        }
+    },
 }
 </script>
