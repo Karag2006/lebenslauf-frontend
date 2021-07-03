804214e001bae33692ce005cd93cb9fc69b40f1b
Martin Richter <martin.richter2006@gmail.com>
2021-07-03 10:46:58 +0200

    style.css, jobItem.vue und jobWorkItem.vue aktualisiert

--- src/components/main/jobItem.vue
+++ src/components/main/jobItem.vue
@@ -1,12 +1,28 @@
 <template>
     <div class="jobs--item">
-        <p>{{item.institution}} / <span class="accent">{{item.timeFrame}}</span></p>
-        <p>{{item.occupation}}</p>
+        <div v-if="!loggedIn" class="jobs--item--head">
+            <p>{{item.institution}} / <span class="accent">{{item.timeFrame}}</span></p>
+            <p>{{item.occupation}}</p>
+        </div>
+        <div v-else-if="!editmode" class="clickable jobs--item--head" @click="edit">
+            <p>{{item.institution}} / <span class="accent">{{item.timeFrame}}</span></p>
+            <p>{{item.occupation}}</p>
+        </div>
+        <edit-item 
+            v-else
+            :names="['institution', 'timeFrame', 'occupation']"
+            :values="item"
+            :location="location"
+            v-on:cancel="cancel"
+            v-on:changed="cancel"
+        ></edit-item>
         <ul>
             <job-work-item
                 v-for="workItem in item.work"
                 :key="workItem.id"
                 :item="workItem"
+                :location="location + '.work.' + workItem.id"
+                :loggedIn="loggedIn"
             ></job-work-item>
         </ul>
     </div>
@@ -14,14 +30,30 @@
 
 <script>
 import jobWorkItem from './jobWorkItem.vue'
+import editItem from '../admin/edit/editItem.vue'
+
 export default {
     components:{
-        jobWorkItem
+        jobWorkItem,
+        editItem
     },
     props:{
         item: Object,
         location: String,
         loggedIn: Boolean
-    }
+    },
+    data() {
+        return {
+            editmode: false
+        }
+    },
+    methods: {
+        edit(){
+            this.editmode = true
+        },
+        cancel(){
+            this.editmode = false
+        }
+    },
 }
 </script>
--- src/components/main/jobWorkItem.vue
+++ src/components/main/jobWorkItem.vue
@@ -1,11 +1,40 @@
 <template>
-    <li> {{item.text}} </li>
+    <li v-if="!loggedIn"> {{item.text}} </li>
+    <li v-else-if="!editmode" class="clickable" @click="edit"> {{item.text}} </li>
+    <edit-item 
+        v-else
+        :names="['text']"
+        :location="location"
+        :values="item"
+        @cancel="cancel"
+        @changed="cancel"
+    ></edit-item>
 </template>
 
 <script>
+import editItem from '../admin/edit/editItem.vue'
+
 export default {
+    components:{
+        editItem
+    },
     props:{
-        item: Object
-    }
+        item: Object,
+        location: String,
+        loggedIn: Boolean
+    },
+    data() {
+        return {
+            editmode: false
+        }
+    },
+    methods: {
+        edit(){
+            this.editmode = true
+        },
+        cancel(){
+            this.editmode = false
+        }
+    },
 }
 </script>
